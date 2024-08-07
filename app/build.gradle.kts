c003e797cdd4ff4fb5c0538329ca8313dd04f149
Ariq Andrean <79554778+Festezy@users.noreply.github.com>
2024-08-07 10:01:36 +0700

    preparation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
