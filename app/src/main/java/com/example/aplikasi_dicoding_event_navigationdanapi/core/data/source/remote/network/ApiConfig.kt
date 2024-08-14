eacb80e18185ca67f5f7f341795f6f99e4fb32ad
Ariq Andrean <79554778+Festezy@users.noreply.github.com>
2024-08-14 21:53:53 +0700

    clean architecture 2(Interface Repository dan Use Case, Logging Interceptor)

--- app/src/main/java/com/example/aplikasi_dicoding_event_navigationdanapi/core/data/source/remote/network/ApiConfig.kt
+++ app/src/main/java/com/example/aplikasi_dicoding_event_navigationdanapi/core/data/source/remote/network/ApiConfig.kt
@@ -5,20 +5,30 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApiConfig {
     companion object{
         private const val BASE_URL = BuildConfig.BASE_URL
-        fun getApiService(): ApiService {
-            val loggingInterceptor =
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-            val client = OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
+
+        private fun provideOkHttpClient(): OkHttpClient {
+            return OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
                 .build()
+        }
+
+        fun getApiService(): ApiService {
+//            val loggingInterceptor =
+//                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+//            val client = OkHttpClient.Builder()
+//                .addInterceptor(loggingInterceptor)
+//                .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
+                .client(provideOkHttpClient())
                 .build()
             return retrofit.create(ApiService::class.java)
         }
