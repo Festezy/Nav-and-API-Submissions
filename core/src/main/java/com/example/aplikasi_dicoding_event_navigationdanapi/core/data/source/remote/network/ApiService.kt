d7492f16f5f1b48b5452c60163dd9970ac863bae
Ariq Andrean <79554778+Festezy@users.noreply.github.com>
2024-08-21 17:16:22 +0700

    revisi berdasarkan Catatan dari Reviewer

--- core/src/main/java/com/example/aplikasi_dicoding_event_navigationdanapi/core/data/source/remote/network/ApiService.kt
+++ core/src/main/java/com/example/aplikasi_dicoding_event_navigationdanapi/core/data/source/remote/network/ApiService.kt
@@ -1,9 +1,7 @@
 package com.example.aplikasi_dicoding_event_navigationdanapi.core.data.source.remote.network
 
-import com.example.aplikasi_dicoding_event_navigationdanapi.core.data.source.remote.response.DetailEventResponse
 import com.example.aplikasi_dicoding_event_navigationdanapi.core.data.source.remote.response.EventResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface ApiService {
 
